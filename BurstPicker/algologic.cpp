47bde2c590264cc013fb9859d6142fdfec3ca85c
beliyugnetatel <reddmk2@gmail.com>
2026-08-26 21:09:46 +0300

    makeBW

---
+++ BurstPicker/algologic.cpp
@@ -0,0 +1,15 @@
+#include "algologic.h"
+
+AlgoLogic::AlgoLogic() {}
+
+
+void AlgoLogic::makeBW(const QString &filePath)
+{
+    QImage Image(filePath);
+    if (Image.format() != QImage::Format_RGB32 && )
+
+    for (int i = 0; i < Image.sizeInBytes(); i++)
+    {
+
+    }
+}
---
+++ BurstPicker/algologic.h
@@ -0,0 +1,17 @@
+#ifndef ALGOLOGIC_H
+#define ALGOLOGIC_H
+
+#include <QString>
+#include <QImage>
+
+class AlgoLogic
+{
+public:
+    AlgoLogic();
+
+private:
+
+    void makeBW(const QString &filePath);
+};
+
+#endif // ALGOLOGIC_H
